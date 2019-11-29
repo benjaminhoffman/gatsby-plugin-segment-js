42a2c1e6765c93dcb29f7df96d8f6a6361311635
Kilian Valkhof <kilian@kilianvalkhof.com>
2019-11-29 19:18:27 +0100

    Add a delayed load function to improve TTI (#19)
    * implement delayed load function to improve TTI
    
    * implement potential load onRouteChange
    
    * update readme
    
    * prevent checking segmentSnippetLoading onRouteUpdate
    
    * add link to info on TTI
    
    * implement customisable delay time, requestIdleCallback and only delay script load, not segment init
    
    * comment the if-statement

--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -1,5 +1,17 @@
-exports.onRouteUpdate = ({}, { trackPage }) => {
+exports.onRouteUpdate = ({ prevLocation }, { trackPage }) => {
+  function trackSegmentPage() {
     if (trackPage) {
-        window.analytics && window.analytics.page();
+      window.analytics && window.analytics.page();
     }
+  }
+
+  // If this is a second page loaded via Gatsby routing, and the delayed loader has 
+  // not been loaded yet, we want to load it and then track the page.
+  if (prevLocation && window.segmentSnippetLoaded === false) {
+    window.segmentSnippetLoader(() => {
+      trackSegmentPage();
+    });
+  } else {
+    trackSegmentPage();
+  }
 };
--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
-    const { trackPage, prodKey, devKey } = pluginOptions;
+    const { trackPage, prodKey, devKey, delayLoad, delayLoadTime } = pluginOptions;
 
     // ensures Segment write key is present
     if (!prodKey || prodKey.length < 10)
@@ -20,16 +20,50 @@ exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
 
     // Segment's minified snippet (version 4.1.0)
     const snippet = `!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.1.0";
-    analytics.load('${writeKey}');
+    ${!delayLoad ? `analytics.load('${writeKey}');` : ``}
     }}();
   `;
+
+    const delayedLoader = `
+      window.segmentSnippetLoaded = false;
+      window.segmentSnippetLoading = false;
+
+      window.segmentSnippetLoader = function (callback) {
+        if (!window.segmentSnippetLoaded && !window.segmentSnippetLoading) {
+          window.segmentSnippetLoading = true;
+
+          function loader() {
+            window.analytics.load('${writeKey}');
+            window.segmentSnippetLoading = false;
+            window.segmentSnippetLoaded = true;
+            if(callback) {callback()}
+          };
+
+          setTimeout(
+            function () {
+              "requestIdleCallback" in window
+                ? requestIdleCallback(function () {loader()})
+                : loader();
+            },
+            ${delayLoadTime} || 1000
+          );
+        }
+      }
+      window.addEventListener('scroll',function () {window.segmentSnippetLoader()}, { once: true });
+    `
+
+    // if delayLoad option is true, use the delayed loader
+    const snippetToUse = `
+      ${delayLoad ? delayedLoader : ''}
+      ${snippet}
+    `
 
     // only render snippet if write key exists
     if (writeKey) {
         setHeadComponents([
             <script
                 key="plugin-segment"
-                dangerouslySetInnerHTML={{ __html: snippet }}
+                dangerouslySetInnerHTML={{ __html: snippetToUse }}
             />
         ]);
     }
