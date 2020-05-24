9e75509fea67caa6d62801811cb84d81d7f41b4d
benjamin hoffman <6520022+benjaminhoffman@users.noreply.github.com>
2020-05-24 10:40:17 -0700

    Update to the page tracking delay
    Cleans up a feature we just merged that delays the page call.  More info below but the short of it is, there's something funky going on with gatsby router that causes it to be out of sync with the actual page, thereby recording the page title incorrectly (it records it as the previous page title).
    
    PR: https://github.com/benjaminhoffman/gatsby-plugin-segment-js/commit/b2a536ad77b9f4324d4dbee030fed9334a9b5fe6

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,9 +5,12 @@ exports.onRouteUpdate = function (_ref, _ref2) {
   var trackPage = _ref2.trackPage;
 
   function trackSegmentPage() {
-    if (trackPage) {
-      window.analytics && window.analytics.page();
-    }
+    window.setTimeout(function () {
+      console.log(document.title);
+      if (trackPage) {
+        window.analytics && window.analytics.page();
+      }
+    }, 50);
   }
 
   if (prevLocation && window.segmentSnippetLoaded === false) {
--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -1,19 +1,23 @@
 exports.onRouteUpdate = ({ prevLocation }, { trackPage }) => {
   function trackSegmentPage() {
-    if (trackPage) {
-      window.analytics && window.analytics.page();
-    }
+    // Adding a 50ms delay ensure that the segment route tracking is in sync with
+    // the actual Gatsby route (otherwise you can end up in a state where the Segment
+    // page tracking reports the previous page on route change).
+    window.setTimeout(() => {
+      if (trackPage) {
+        window.analytics && window.analytics.page();
+      }
+    }, 50);
   }
 
   // This `if/then` logic relates to the `delayLoad` functionality to help prevent
-  // calling `trackPage` twice. If you don't use that feature, you can ignore. Here
-  // call `trackPage` only _after_ we change routes (on the client).
-  // Adding a 50ms delay ensure that the segment route tracking is in sync with the actual Gatsby route. Otherwise you can end up in a state where the Segment page tracking reports the previous page on route change.
+  // calling `trackPage` twice. If you don't use that feature, you can ignore.
+  // Here call `trackPage` only _after_ we change routes (on the client).
   if (prevLocation && window.segmentSnippetLoaded === false) {
     window.segmentSnippetLoader(() => {
-      window.setTimeout(trackSegmentPage, 50)
+      trackSegmentPage();
     });
   } else {
-    window.setTimeout(trackSegmentPage, 50)
+    trackSegmentPage();
   }
 };
