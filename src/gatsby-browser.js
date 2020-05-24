b2a536ad77b9f4324d4dbee030fed9334a9b5fe6
Tom Pridham <pridham.tom@gmail.com>
2020-05-24 11:06:53 -0600

    add delay to page tracking call (#30)

--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -8,11 +8,12 @@ exports.onRouteUpdate = ({ prevLocation }, { trackPage }) => {
   // This `if/then` logic relates to the `delayLoad` functionality to help prevent
   // calling `trackPage` twice. If you don't use that feature, you can ignore. Here
   // call `trackPage` only _after_ we change routes (on the client).
+  // Adding a 50ms delay ensure that the segment route tracking is in sync with the actual Gatsby route. Otherwise you can end up in a state where the Segment page tracking reports the previous page on route change.
   if (prevLocation && window.segmentSnippetLoaded === false) {
     window.segmentSnippetLoader(() => {
-      trackSegmentPage();
+      window.setTimeout(trackSegmentPage, 50)
     });
   } else {
-    trackSegmentPage();
+    window.setTimeout(trackSegmentPage, 50)
   }
 };
