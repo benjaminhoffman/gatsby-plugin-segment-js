f664740e75589a6112174e95526340b8d5565abc
newhouse <c.newhouse@gmail.com>
2022-02-05 18:48:36 -0800

    sometimes insignificant inconsistencies

--- tests/shared/tests.test.js
+++ tests/shared/tests.test.js
@@ -18,6 +18,13 @@ if (!appJsMapFileName) {
 }
 const appJsMapFilePath = path.resolve(pathToPublic, appJsMapFileName)
 
+// As Node versions and Gatsby versions etc change + the fact that there is not lock file, things may
+// change a little bit in insignificant ways...so let's remove the whitespace chars before doing
+// comparisons
+function removeWhitespace (str) {
+  return str.replace(/\s+/g, '')
+}
+
 describe('Code is there', function () {
   it('has snippet in the <head>', function () {
     const indexText = fs.readFileSync(indexFilePath).toString()
@@ -42,8 +49,8 @@ describe('Code is there', function () {
   })
 
   it('has segment code in app-*.js.map', function () {
+    const appCode = String.raw`if (!trackPage) {\n    return;\n  }\n\n  function trackSegmentPage() {\n    var delay = Math.max(0, trackPageDelay);\n\n    window.setTimeout(function () {\n      window.analytics && window.analytics.page(document.title);\n    }, delay);\n  }\n\n  if (prevLocation && window.segmentSnippetLoaded === false) {\n    window.segmentSnippetLoader(function () {\n      trackSegmentPage();\n    });\n  } else {\n    trackSegmentPage();\n  }\n};`
     const jsMapText = fs.readFileSync(appJsMapFilePath).toString()
-    console.log(jsMapText)
-    expect(jsMapText).to.include(String.raw`if (!trackPage) {\n    return;\n  }\n\n  function trackSegmentPage() {\n    var delay = Math.max(0, trackPageDelay);\n\n    window.setTimeout(function () {\n      window.analytics && window.analytics.page(document.title);\n    }, delay);\n  }\n\n  if (prevLocation && window.segmentSnippetLoaded === false) {\n    window.segmentSnippetLoader(function () {\n      trackSegmentPage();\n    });\n  } else {\n    trackSegmentPage();\n  }\n};`)
+    expect(removeWhitespace(jsMapText)).to.include(removeWhitespace(appCode))
   })
 })
