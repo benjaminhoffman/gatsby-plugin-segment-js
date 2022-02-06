2728400a81026e08daaa3ac7299a4499bca4a4b7
newhouse <c.newhouse@gmail.com>
2022-02-05 17:54:19 -0800

    comments

--- babel-browser.config.js
+++ babel-browser.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   extends: "./babel.config",
   presets: [
-    // TODO: Figure out if we should specify some browser targets here, or if Gatsby will re-transpile
-    // them or what.
+    // TODO: Figure out if we should specify some browser targets here, or if Gatsby will re-transpile them or what.
     "@babel/preset-env",
     "@babel/preset-react",
   ],
