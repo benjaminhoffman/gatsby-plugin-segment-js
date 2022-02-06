a79a0df4b6c578e0e7683b0a7cf45f2673787d38
newhouse <c.newhouse@gmail.com>
2022-02-05 17:39:22 -0800

    better transpilation

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -1,7 +1,6 @@
 import React from "react";
 
-// Gavin Johnson, 20220205: Changed over to an ES export so ES & CJS aren't mixed
-export function onRenderBody({ setHeadComponents }, pluginOptions) {
+export const onRenderBody = ({ setHeadComponents }, pluginOptions) => {
   const {
     trackPage,
     prodKey,
@@ -31,8 +30,7 @@ export function onRenderBody({ setHeadComponents }, pluginOptions) {
   } else {
     // Segment's snippet (version 4.15.3)
     // If this is updated, probably good to update the README to include the version
-    snippet = `!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="${host}/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="${writeKey}";;analytics.SNIPPET_VERSION="4.15.3";`
-    
+    snippet = `!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="${host}/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="${writeKey}";analytics.SNIPPET_VERSION="4.15.3";`
     // Should it be loaded right away?
     if (!(delayLoad || manualLoad)) {
       snippet += `\nanalytics.load('${writeKey}');`
